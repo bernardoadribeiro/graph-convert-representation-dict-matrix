a07848c9a3e524238694fe5e0c422ce1cc06cb51
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-10-18 09:48:16 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Graph():
+
+    def dict_to_matrix(self, _graph: dict):
+        """
+            _graph: dict that represet the graph that will be converted into adjacency matrix
+        """
+        pass
+
+    def matrix_to_dict(self, matrix_adj):
+        pass
+
+
+if __name__ == '__main__':
+    print("Hello World!")
